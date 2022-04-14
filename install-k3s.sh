efc1ca61c31e4f139fa1b5bf58569034edcbb610
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-04-14 17:02:11 +0800

    Update install-k3s.sh

--- install-k3s.sh
+++ install-k3s.sh
@@ -8,4 +8,6 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 k3s check-config
 
 ### 国内安装k3s
-#curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -
+# curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -
+
+# curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -s - --disable=traefik
