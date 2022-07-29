bafb8298dcba9e2b6beded8d17db9baec937897d
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-07-29 19:50:36 +0800

    Update cluster-api.sh

--- cluster-api.sh
+++ cluster-api.sh
@@ -16,3 +16,17 @@ nodes:
 
 kind create cluster --config mgmt-cluster-config.yaml --name mgmt
 
+clusterctl init --infrastructure docker
+
+
+kubectl apply -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+clusterctl generate cluster c1 --flavor development \
+  --infrastructure docker \
+  --kubernetes-version v1.21.1 \
+  --control-plane-machine-count=3 \
+  --worker-machine-count=3 \
+  > c1-clusterapi.yaml
+  
+
+/mgmt/templates/cluster-api-template.yaml
