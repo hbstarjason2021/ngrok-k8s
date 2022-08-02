552fcdde56d248a5701ccbc0c279b648825ec562
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-08-02 11:16:05 +0800

    Update init-tools.sh

--- init-tools.sh
+++ init-tools.sh
@@ -137,6 +137,22 @@ function install_clusterctl(){
     echo -e "${green}clusterctl is already installed${white}"
 }
 
+## https://krew.sigs.k8s.io/docs/user-guide/setup/install/
+function install_krew(){
+    echo "Installing krew"
+    set -x; cd "$(mktemp -d)" && \
+    OS="$(uname | tr '[:upper:]' '[:lower:]')" && \
+    ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" && \
+    KREW="krew-${OS}_${ARCH}" &&\
+    curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" && \
+    tar zxvf "${KREW}.tar.gz" && \
+    ./"${KREW}" install krew
+    cp ~/.krew/bin/kubectl-krew /usr/local/bin/
+    export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+    kubectl krew version
+    echo -e "${green}krew is already installed${white}"
+}
+
 ## install_git
 ## install_docker
 install_kubectl
@@ -148,4 +164,5 @@ install_compose
 install_kind
 install_terraform
 install_clusterctl
+install_krew
 
