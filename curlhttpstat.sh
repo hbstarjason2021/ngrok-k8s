2a33acf0ced1d1ef2efa2a880ff911a5eb67eb8b
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2022-09-15 22:23:50 +0800

    Create curlhttpstat.sh

---
+++ curlhttpstat.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+## bash curlhttpstat.sh www.baidu.com
+
+curl -Ss -w'Timeline:
+|
+|--NAMELOOKUP %{time_namelookup}
+|--|--CONNECT %{time_connect}
+|--|--|--APPCONNECT %{time_appconnect}
+|--|--|--|--PRETRANSFER %{time_pretransfer}
+|--|--|--|--|--STARTTRANSFER %{time_starttransfer}
+|--|--|--|--|--|--TOTAL %{time_total}
+|--|--|--|--|--|--REDIRECT %{time_redirect}
+
+Speed: %{speed_download} Bytes/s
+' -o /dev/null $1
