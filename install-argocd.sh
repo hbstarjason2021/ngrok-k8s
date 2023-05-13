5e5d067323f797222ab3f8892e17327ec09568f6
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-05-13 16:26:15 +0800

    Update install-argocd.sh

--- install-argocd.sh
+++ install-argocd.sh
@@ -8,3 +8,7 @@ kubectl get secret argocd-initial-admin-secret \
     --output jsonpath="{.data.password}" \
     | base64 --decode \
     && echo
+
+### kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+
+### https://killercoda.com/mabusaa/course/argocd-endusers-scenarios/01-setting-up-argocd
