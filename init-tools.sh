4ef241b375a114acefbd7a12d2224ad24426b0cb
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2023-10-10 09:32:45 +0800

    Update init-tools.sh

--- init-tools.sh
+++ init-tools.sh
@@ -229,7 +229,7 @@ function install_kind(){
 
 function install_terraform(){
     echo "Installing terraform"
-    TERRAFORM_VESION="1.4.6"
+    TERRAFORM_VESION="1.6.0"
     curl -LO https://releases.hashicorp.com/terraform/${TERRAFORM_VESION}/terraform_${TERRAFORM_VESION}_linux_amd64.zip
     unzip terraform_${TERRAFORM_VESION}_linux_amd64.zip
     chmod +x ./terraform
